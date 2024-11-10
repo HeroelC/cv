516110fdf527b6fb7b5981e0b23be6e84f0faf83
Heroel <heroeljcarpinetti@gmail.com>
2024-11-10 09:14:27 -0300

    Panel en cart

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ async function optionMenuDev() {
                         const fecha2 = new Date(); // Fecha de hoy
                         
                         calculateDateDifference(fecha1, fecha2);
+                        likeOption();
                         break;
                     case 'Experiencia Profesor':
                         request = await fetch('./content/professor.html').then(response => {
@@ -79,7 +80,7 @@ async function initialInfo(){
     const fecha2 = new Date(); // Fecha de hoy
     
     calculateDateDifference(fecha1, fecha2);
-
+    likeOption();
     let optionExperience = document.getElementById('Experiencia');
     optionExperience.classList.add('selected');
 }
@@ -94,7 +95,7 @@ function calculateDateDifference(date1, date2) {
     }
     
     document.querySelectorAll(".current-time").forEach( item => item.innerHTML = `${yearDiff} año y ${monthDiff} meses`);
-  }
+}
 
 function darkMode(){
 
@@ -114,4 +115,31 @@ toggleButton.addEventListener("click", function() {
     toggleButton.innerHTML = '<svg class="svg-style sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>';
   }
 });
+}
+
+function likeOption(){
+    let likeOption = document.querySelectorAll('.like-option');
+    likeOption.forEach(likeAction => {
+        likeAction.addEventListener('click', (e) => {
+            let span = likeAction.getElementsByTagName('span')[0];
+            if (likeAction.attributes.title.value == "Me gusta"){ 
+                likeAction.attributes.title.value = "Cancelar Me gusta";
+                likeAction.classList.add('active');
+                span.innerHTML = parseInt(span.innerHTML) + 1;
+
+                span.classList.add('update');
+                setTimeout(() => {
+                    span.classList.remove('update');
+                }, 500);
+            }else{
+                likeAction.attributes.title.value = "Me gusta"; 
+                likeAction.classList.remove('active');
+                span.innerHTML = parseInt(span.innerHTML) - 1;
+                span.classList.add('update');
+                setTimeout(() => {
+                    span.classList.remove('update');
+                }, 500);
+            }
+        });
+    });
 }
